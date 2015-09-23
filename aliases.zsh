dd379f415c001e90a6a569356535a120f0dae2c7
Cole Peters <cole@colepeters.com>
2015-09-23 09:07:47 +0100

    Update aliases for nvim and git

--- aliases.zsh
+++ aliases.zsh
@@ -1,76 +1,30 @@
-# These aliases adopted from YADR
-# https://github.com/skwp/dotfiles
-
-# vim using
+# vim
 mvim --version > /dev/null 2>&1
 MACVIM_INSTALLED=$?
 if [ $MACVIM_INSTALLED -eq 0 ]; then
   alias vim="mvim -v"
 fi
-
-# vimrc editing
 alias ve='vim ~/.vimrc'
 
-# zsh profile editing
+# nvim
+alias nv='nvim'
+alias ne='nvim ~/.nvimrc'
+
+# zsh
 alias ze='vim ~/.zshrc'
 alias zr='source ~/.zshrc'
 
-# Git Aliases
+# git
 alias gs='git status'
-alias gstsh='git stash'
-alias gst='git stash'
-alias gsp='git stash pop'
-alias gsa='git stash apply'
-alias gsh='git show'
-alias gshw='git show'
-alias gshow='git show'
-alias gi='vim .gitignore'
 alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
 alias gco='git co'
-alias gcp='git cp'
-alias ga='git add -A'
-alias guns='git unstage'
-alias gunc='git uncommit'
-alias gm='git merge'
-alias gms='git merge --squash'
-alias gam='git amend --reset-author'
-alias grv='git remote -v'
-alias grr='git remote rm'
-alias grad='git remote add'
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias ggrc='git rebase --continue'
-alias gbi='git rebase --interactive'
 alias gl='git l'
-alias glg='git l'
-alias glog='git l'
-alias co='git co'
-alias gf='git fetch'
-alias gfch='git fetch'
 alias gd='git diff'
-alias gb='git b'
-alias gbd='git b -D -w'
-alias gdc='git diff --cached -w'
-alias gpub='grb publish'
-alias gtr='grb track'
 alias gpl='git pull'
-alias gplr='git pull --rebase'
 alias gps='git push'
-alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias gnb='git nb' # new branch aka checkout -b
-alias grs='git reset'
-alias grsh='git reset --hard'
-alias gcln='git clean'
-alias gclndf='git clean -df'
-alias gclndfx='git clean -dfx'
 alias gsm='git submodule'
 alias gsmi='git submodule init'
 alias gsmu='git submodule update'
-alias gt='git t'
-alias gbg='git bisect good'
-alias gbb='git bisect bad'
 alias ts='tig status'
 
 # Finder
