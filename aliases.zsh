f39d7e9f85d13f10400fe851432a81a8ea5b58c9
Cole Peters <cole@colepeters.com>
2016-02-28 02:10:04 +0000

    Remove unused Zsh aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,20 +1,12 @@
-# vim
-mvim --version > /dev/null 2>&1
-MACVIM_INSTALLED=$?
-if [ $MACVIM_INSTALLED -eq 0 ]; then
-  alias vim="mvim -v"
-fi
-alias ve='vim ~/.vimrc'
+### NEOVIM ###
+alias nv='nvim'                             # nv is quicker to write than nvim
+alias ne='nvim ~/.config/nvim/init.vim'     # Quick access to Neovim config
 
-# nvim
-alias nv='nvim'
-alias ne='nvim ~/.config/nvim/init.vim'
+### ZSH ###
+alias ze='nv ~/.zshrc'                      # Quick access to Zsh config
+alias zr='source ~/.zshrc'                  # Reload Zsh config
 
-# zsh
-alias ze='vim ~/.zshrc'
-alias zr='source ~/.zshrc'
-
-# git
+### GIT ###
 alias gs='git status'
 alias gcm='git ci -m'
 alias gco='git co'
@@ -23,14 +15,14 @@ alias gpl='git pull'
 alias gps='git push'
 alias ts='tig status'
 
-# Homebrew
+### HOMEBREW ###
 alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
 
-# Node/NPM
+### NPM ###
 alias ffs='rm -rf node_modules/ && npm cache clean && npm i'
 
-# HTTPie, always with Solarized colours
+### HTTPIE ###
 alias http='http --style=solarized'
 
-# Call user aliases
+### GIMME USER’S PRIVATE ALIASES PLZ ###
 source ~/.aliases.user.zsh
