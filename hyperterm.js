835904b854fa8f24d461b9540c132d36b9a6bc24
Cole Peters <cole@colepeters.com>
2016-08-19 14:32:13 +0100

    Increase hyperterm font size

--- hyperterm.js
+++ hyperterm.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily: '"SF Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
