a8be048ff6d10ba79cf186f485efe7c134f3de2d
Cole Peters <cole@colepeters.com>
2022-10-21 17:15:30 -0500

    Update bufferline highlights

--- config/nvim/lua/colepeters/plugins/bufferline.lua
+++ config/nvim/lua/colepeters/plugins/bufferline.lua
@@ -1,1 +1,50 @@
-require('bufferline').setup()
+local palette = require('nightfox.palette').load('dayfox')
+
+require('bufferline').setup({
+  options = {
+    indicator = {
+      style = 'none'
+    },
+    separator_style = 'padded_slant',
+  },
+  -- highlights for Dayfox
+  highlights = {
+    fill = {
+      bg = palette.bg0,
+    },
+    separator = {
+      fg = palette.bg0,
+      bg = palette.bg4,
+    },
+    separator_selected = {
+      fg = palette.bg0,
+      bg = palette.bg1,
+    },
+    separator_visible = {
+      fg = palette.bg0,
+      bg = palette.bg1,
+    },
+    background = {
+      fg = palette.fg0,
+      bg = palette.bg4,
+    },
+    buffer_selected = {
+      italic = false,
+    },
+    buffer_visible = {
+      fg = palette.fg0,
+      bg = palette.bg1,
+    },
+    close_button = {
+      fg = palette.fg0,
+      bg = palette.bg4,
+    },
+    close_button_visible = {
+      fg = palette.fg0,
+    },
+    modified = {
+      bg = palette.bg4,
+      fg = palette.fg0,
+    },
+  }
+})
