a82f4979ba2906a95467800d3467c9735d73d7aa
Cole Peters <cole@colepeters.com>
2022-11-14 17:29:18 -0400

    Don't force Prettier on save:
    - Prettier should really be run via eslint-plugin-prettier, rather
      than as a standalone formatter

--- config/nvim/lua/colepeters/plugins/null-ls.lua
+++ config/nvim/lua/colepeters/plugins/null-ls.lua
@@ -19,14 +19,6 @@ require('null-ls').setup({
     require('null-ls').builtins.code_actions.eslint_d,
     require('null-ls').builtins.diagnostics.eslint_d,
     require('null-ls').builtins.formatting.eslint_d,
-    require('null-ls').builtins.formatting.prettierd.with({
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'typescript',
-        'typescriptreact',
-      },
-    }),
   },
 })
 
