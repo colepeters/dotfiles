09b834b92b0625b0ac9e6d73e5286707a218c4f1
Cole Peters <cole@colepeters.com>
2024-10-18 10:59:25 -0500

    Disable signature helper: - https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/41

--- config/nvim/lua/colepeters/plugins/nvim-cmp.lua
+++ config/nvim/lua/colepeters/plugins/nvim-cmp.lua
@@ -66,7 +66,7 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'nvim_lsp_signature_help' },
+    -- { name = 'nvim_lsp_signature_help' },
     { name = 'nvim_lua'},
     { name = 'path' },
   }),
